40093b06a7b45beea9c9756e30123e8ad6ef6990
rradeba <rradeba@gmail.com>
2024-09-12 13:46:58 -0400

    c1

--- src/assets/main.jsx
+++ src/assets/main.jsx
@@ -9,9 +9,9 @@ function Main() {
     <div className="main">
       <MyNavbar />  {/* Use the custom navbar component */}
       <div className="card">
-        <h1>Crypto Market</h1>
-        <p>Find new crypto coins in our directory.</p>
-        <img className="image img-fluid" src="/book.png" alt="Crypto" />
+        <h1>Reid's Book Market</h1>
+        <p>Management App For Reid's Books</p>
+        <img className="image img-fluid" src="\book.png" alt="Crypto" />
         <Button className="custom-button mb-2">
           Shop Now
         </Button>
