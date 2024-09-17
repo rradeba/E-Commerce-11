2cf1b44a8abdc8bae3fc55f33c089d9ae51475b0
rradeba <rradeba@gmail.com>
2024-09-17 13:41:58 -0400

    c1

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,12 @@
-import React from 'react'; 
-import MyNavbar from './navbar.jsx';  // Import your custom navbar component
-import 'bootstrap/dist/css/bootstrap.min.css';  // Import Bootstrap CSS
-import './main.css'; // Custom CSS for Main component
-import { Button } from 'react-bootstrap';  // Import Bootstrap's Button component
+// main.jsx
+import React from 'react';
 
-function Main() { 
+const Main = () => {
   return (
-    <div className="mainPage">
-      <MyNavbar />  
-      <div className="card">
-        <h1>Reid's Book Market</h1>
-        <p>Management App For Reid's Books</p>
-        <img className="image img-fluid" src="\book.png" alt="Crypto" />
-        <Button className="custom-button mb-2">
-          Shop Now
-        </Button>
-      </div>
+    <div>
+      <h1>Hello World</h1>
     </div>
   );
-}
+};
 
 export default Main;
