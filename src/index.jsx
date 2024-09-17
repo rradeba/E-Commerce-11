c2fbb9aef0ef44657f66c5697913789a6cdd8fba
rradeba <rradeba@gmail.com>
2024-09-17 13:43:40 -0400

    c1

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';  // Import createRoot from 'react-dom/client'
-import './index.css'; 
 import Main from './main.jsx';
 import reportWebVitals from './reportWebVitals';
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,23 @@
-// main.jsx
-import React from 'react';
+import React from 'react'; 
+import MyNavbar from './navbar.jsx';  // Import your custom navbar component
+import 'bootstrap/dist/css/bootstrap.min.css';  // Import Bootstrap CSS
+import './main.css'; // Custom CSS for Main component
+import { Button } from 'react-bootstrap';  // Import Bootstrap's Button component
 
-const Main = () => {
+function Main() { 
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="mainPage">
+      <MyNavbar />  
+      <div className="card">
+        <h1>Reid's Book Market</h1>
+        <p>Management App For Reid's Books</p>
+        <img className="image img-fluid" src="\book.png" alt="Crypto" />
+        <Button className="custom-button mb-2">
+          Shop Now
+        </Button>
+      </div>
     </div>
   );
-};
+}
 
 export default Main;
